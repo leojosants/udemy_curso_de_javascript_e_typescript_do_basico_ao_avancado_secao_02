834c773486706cbd0cf8ac944f913a88c6820a13
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-18 12:11:05 -0300

    class 18 - let vs var: first difference

---
+++ 18_let_vs_var_primeira_diferenca/app.js
@@ -0,0 +1,9 @@
+/* SyntaxError: Identifier 'name_0' has already been declared */
+// let name_0 = 'Leonardo';
+// let name_0 = 'Leonardo';
+
+var name_1 = 'Leonardo';
+console.log(name_1);
+
+var name_1 = 'João';
+console.log(name_1);
