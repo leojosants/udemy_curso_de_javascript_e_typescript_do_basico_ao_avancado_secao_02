86bfd35e7b3ea986bc4c3d8f1159981e7f7c46c5
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-18 14:01:11 -0300

    class 20 - arithmetic, assignment and increment operators

---
+++ 20_operadores_aritmeticos_de_atribuicao_e_incremento/app.js
@@ -0,0 +1,140 @@
+/*
+    arithmetic operators
+        +   addition / concatenation
+        -   subtraction
+        *   multiplication
+        /   division
+        **  potentiation
+        %   modulo
+
+        order of precedence
+            ()      parentheses
+            **      exponentiation
+            * / %   multiplication, division and modulo
+            + -     addition and subtraction
+
+    increment / decrement operators
+        ++  increment operator
+        --  decrement operator
+
+    assignment operators
+        +=
+        -=
+        *=
+        /=
+        %=
+        **=
+*/
+
+// addition    +
+/*
+    const number_0 = 5;
+    const number_1 = 10;
+    const result_addition = number_0 + number_1;
+    console.log(`Resultado: ${number_0} + ${number_1} = ${result_addition}`);
+*/
+
+// concatenation   +
+/*
+    const number_0_string = '5';
+    const number_1_string = '10';
+    const concatenation = number_0_string + number_1_string;
+    console.log(`Resultado: ${number_0_string} + ${number_1_string} = ${concatenation}`);
+*/
+
+// subtraction -
+/*
+    const number_2 = 25;
+    const number_3 = 10;
+    const result_subtraction = number_2 - number_3;
+    console.log(`Resultado: ${number_2} - ${number_3} = ${result_subtraction}`);
+*/
+
+// multiplication  *
+/*
+    const number_4 = 25;
+    const number_5 = 10;
+    const result_multiplication = number_4 * number_5;
+    console.log(`Resultado: ${number_4} * ${number_5} = ${result_multiplication}`);
+*/
+
+//  *   division
+/*
+    const number_6 = 25;
+    const number_7 = 10;
+    const result_division = number_6 / number_7;
+    console.log(`Resultado: ${number_6} / ${number_7} = ${result_division}`);
+*/
+
+//  **  potentiation
+/*
+    const base_number = 2;
+    const exponent_number = 10;
+    const result_pow = base_number ** exponent_number;
+    console.log(`Resultado: ${base_number} ** ${exponent_number} = ${result_pow}`);
+*/
+
+//  %  module
+/*
+    const number_8 = 30;
+    const number_9 = 10;
+    const result_module = number_8 % number_9;
+    console.log(`Resultado: ${number_8} % ${number_9} = ${result_module}`);
+*/
+
+// ++  increment operator
+/*
+    let counter_increment_pos_0 = 0;
+    console.log(counter_increment_pos_0);
+    console.log(counter_increment_pos_0++);
+    console.log(counter_increment_pos_0);
+
+    let counter_increment_pre_0 = 0;
+    console.log(counter_increment_pre_0);
+    console.log(++counter_increment_pre_0);
+*/
+
+// --  decrement operator
+/*
+    let counter_decrement_pos_1 = 10;
+    console.log(counter_decrement_pos_1);
+    console.log(counter_decrement_pos_1--);
+    console.log(counter_decrement_pos_1);
+
+    let counter_decrement_pre_1 = 10;
+    console.log(counter_decrement_pre_1);
+    console.log(--counter_decrement_pre_1);
+*/
+
+// assignment operators +=  -=  *=  /=  %=  **=
+/*
+    // +=
+    let counter_0 = 40;
+    counter_0 += 10;
+    console.log(counter_0);
+
+    // -=
+    let counter_1 = 40;
+    counter_1 -= 10;
+    console.log(counter_1);
+
+    // *=
+    let counter_2 = 40;
+    counter_2 *= 10;
+    console.log(counter_2);
+
+    // /=
+    let counter_3 = 40;
+    counter_3 /= 10;
+    console.log(counter_3);
+
+    // %=
+    let counter_4 = 40;
+    counter_4 %= 10;
+    console.log(counter_4);
+
+    // **=
+    let counter_5 = 2;
+    counter_5 **= 10;
+    console.log(counter_5);
+*/
