8563bfe2250b761409b88685fc1d7652fe63225f
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-18 12:30:20 -0300

    class 19 - primitive data types

---
+++ 19_tipos_de_dados_primitivos/app.js
@@ -0,0 +1,27 @@
+/* string */
+const name_0 = 'Leonardo';
+const name_1 = "Leonardo";
+const name_2 = `Leonardo`;
+console.log(typeof name_0, name_0);
+console.log(typeof name_1, name_1);
+console.log(typeof name_2, name_2);
+
+/* number */
+const number_0 = 10;
+const number_1 = 10.52;
+console.log(typeof number_0, number_0);
+console.log(typeof number_1, number_1);
+
+/* undefined */
+let name_student;
+console.log(typeof name_student, name_student);
+
+/* null */
+const lastname_student = null;
+console.log(typeof lastname_student, lastname_student);
+
+/* boolean */
+const approved_student = true;
+const failed_student = false;
+console.log(typeof approved_student, approved_student);
+console.log(typeof failed_student, failed_student);
