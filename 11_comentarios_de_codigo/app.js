fa1ae71da5ab0da5d5aedde0e587cf52df7e54f9
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-18 10:50:20 -0300

    class 11 - code comments

---
+++ 11_comentarios_de_codigo/app.js
@@ -0,0 +1,8 @@
+// Este é um comentário de única linha
+
+// Tudo que estiver na frente de (//), não será executado
+
+/*
+    Este é um comentário de bloco - múltiplas linhas
+    Tudo que estiver dentro de (/* * /) não será executado
+*/
