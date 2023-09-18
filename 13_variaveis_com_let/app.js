de722cf4b91f5e558cd4703edc8c9c79d9f4de67
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-18 11:26:55 -0300

    class 13 - variables with let

---
+++ 13_variaveis_com_let/app.js
@@ -0,0 +1,8 @@
+let name_joao = 'João';
+let name_maria = 'Maria';
+
+console.log(`${name_joao} nasceu em`, 1984 + '.');
+console.log('Em', 2000, ` ${name_joao} conheceu ${name_maria}.`);
+console.log(`${name_joao} se casou com ${name_maria} em `, 2012 + '.');
+console.log(`${name_maria} teve`, 1, ` filho com ${name_joao} em`, 2015 + '.');
+console.log(`O filho de ${name_joao} se chama Eduardo.`);
